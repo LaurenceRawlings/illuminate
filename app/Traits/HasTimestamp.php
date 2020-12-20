e0afc75aaba19624cf5c94046b8914115065ef29
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-12-20 20:51:24 +0000

    Fix timestamp for news posts

--- app/Traits/HasTimestamp.php
+++ app/Traits/HasTimestamp.php
@@ -4,9 +4,16 @@
 namespace App\Traits;
 
 
+use App\Models\NewsPost;
+
 trait HasTimestamp
 {
     public function getTimestampAttribute() {
+
+        if ($this instanceof NewsPost) {
+            return $this->published_at->diffForHumans();
+        }
+
         return $this->created_at->diffForHumans();
     }
 }
