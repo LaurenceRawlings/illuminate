39b56608df1a97756d16385444711acada17b48b
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-12-23 21:36:10 +0000

    Display 'You' instead of name for the logged in user

--- resources/js/Shared/Components/ProfileLink.vue
+++ resources/js/Shared/Components/ProfileLink.vue
@@ -1,6 +1,6 @@
 <template>
     <inertia-link :href="route('user.show', user.username)" class="text-sm font-bold text-gray-700 hover:underline">
-        {{ user.name }}
+        {{ $page.user.id == user.id ? 'You' : user.name }}
     </inertia-link>
 </template>
 
