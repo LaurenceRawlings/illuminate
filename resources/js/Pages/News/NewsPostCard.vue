71fec05c0f9b3bf4dd887b2d51d13cbbe37742a3
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-12-24 00:18:59 +0000

    Add links to news source homepages

--- resources/js/Pages/News/NewsPostCard.vue
+++ resources/js/Pages/News/NewsPostCard.vue
@@ -3,13 +3,15 @@
         <div :style="backgroundImage(newsPost.thumbnail_url)" class="bg-cover bg-center h-56 p-4"></div>
         <div class="bg-white p-4 rounded-t-lg mt--10 h-full">
             <div class="flex items-center">
-                <div :style="backgroundImage(newsPost.favicon)"
-                     class="bg-cover bg-center w-10 h-10 rounded-full mr-3"></div>
+                <a :href="host" class="text-sm font-bold text-gray-700 hover:underline" rel="noopener noreferrer"
+                   target="_blank">
+                    <div :style="backgroundImage(newsPost.favicon)"
+                         class="bg-cover bg-center w-10 h-10 rounded-full mr-3"></div>
+                </a>
                 <div>
-                    <p class="tracking-wide text-sm font-bold text-gray-700">{{ newsPost.source }}</p>
-                    <p class="text-sm text-gray-700">
-                        {{ newsPost.timestamp }}
-                    </p>
+                    <a :href="host" class="text-sm font-bold text-gray-700 hover:underline" rel="noopener noreferrer"
+                       target="_blank">{{ newsPost.source }}</a>
+                    <p class="text-sm text-gray-700">{{ newsPost.timestamp }}</p>
                 </div>
             </div>
             <p class="text-2xl text-gray-900">{{ newsPost.title }}</p>
@@ -27,6 +29,12 @@ export default {
     },
     methods: {
         ...methods,
+    },
+    computed: {
+        host() {
+            let url = new URL(this.newsPost.url);
+            return `${url.protocol}${url.hostname}`
+        }
     }
 }
 </script>
