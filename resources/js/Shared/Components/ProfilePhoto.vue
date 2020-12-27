e19b1f397847263dd90d9399c399d107511056c3
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-12-27 22:49:17 +0000

    Add border to profile pictures

--- resources/js/Shared/Components/ProfilePhoto.vue
+++ resources/js/Shared/Components/ProfilePhoto.vue
@@ -2,7 +2,8 @@
     <div>
         <inertia-link :href="route('user.show', user.username)">
             <img :src="user ? user.profile_photo_url : undefined"
-                 :alt="user ? user.username : 'User Photo'" class="rounded-full h-full w-full object-cover">
+                 :alt="user ? user.username : 'User Photo'"
+                 class="rounded-full h-full w-full object-cover border-2 border-solid border-gray-200">
         </inertia-link>
     </div>
 </template>
