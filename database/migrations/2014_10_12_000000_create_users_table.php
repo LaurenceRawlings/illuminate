a63378d50671270393ce82727bf1d4e8c428c2a1
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-12-28 18:27:25 +0000

    Add admin field to the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
             $table->string('username', 25)->unique();
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
