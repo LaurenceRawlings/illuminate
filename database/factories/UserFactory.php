826aec7db50ad910a3ec092955aa75f508991620
Laurence Rawlings <rawlingslaurence@gmail.com>
2021-01-01 20:43:01 +0000

    Fix user creation date in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'created_at' => Carbon::parse($this->faker->dateTimeThisMonth()),
+            'created_at' => Carbon::parse($this->faker->dateTimeBetween($startDate = '-2 months', $endDate = '-1 month')),
         ];
     }
 }
